3f0fbe8bdb50463215cae585ca555140365c287f
shraddha-o <44389326+shraddha-o@users.noreply.github.com>
2024-12-26 15:33:30 +0530

    Update validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "Starting Apache server..."
-sudo systemctl start httpd
-sudo systemctl enable httpd
+echo "Validating application..."
+STATUS=$(curl -o /dev/null -s -w "%{http_code}" http://localhost/contact-us.html)
+
+if [ $STATUS -eq 200 ]; then
+  echo "Application is running successfully."
+  exit 0
+else
+  echo "Application validation failed."
+  exit 1
+fi
